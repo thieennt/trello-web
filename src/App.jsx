299b02c15288790521568b14c2f9230a93836e60
Thien Nguyen <thieennt@gmail.com>
2023-08-03 16:10:27 +0700

    Add dark light mode #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme,
+} from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
